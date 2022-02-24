1eed065663f9940098cc97548330961df05f6175
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-02-24 14:51:37 +0000

    add computer randomize function ro make the computer player to choose randomly

--- run.py
+++ run.py
@@ -6,21 +6,30 @@ def starting_game():
     Creating an input to accept r,p or s as a player answer
     return as invalid data if data provided is anything else
     """
-    print("Welcome to the game of Rock, paper and scissors.")
+    print("Welcome to the game of Rock, paper and scissors.\n")
     print("rules: rock beat scissors, paper beats rock and scissors beats paper. \n")
     print("Play by entering r for rock, p for paper and s for scissors.")
 
     try:
+        available_choices = ["r", "p", "s"]
         player_input = input("Enter your choice: ")
-        if player_input == ("r" or "p" or "s"):
+        if any(player_input in choice for choice in available_choices):
             print(f"You chose {player_input}")
+            computer_randomize()
         # need to call function to continue game later
         else:
-            pass
-    except ValueError as e:
-        print(f"Invalid data input: {e}, Please enter r for rock p for paper or s for scissors!")
+            raise ValueError()
+    except ValueError:
+        print(f"Invalid data input: {player_input}, is not a valid input.\n Please enter r for rock p for paper or s for scissors!")
 
-    computer = random.choice(["r", "p", "s"])   # searched for a way to choose one random item from a list and found the random method at pynative.com
-    print(f"computer choise is: {computer}")
+
+def computer_randomize():
+    """
+    using random method from imported random to make the computer choose rock, paper or scissors
+    """
+    computer_choice = random.choice(["r", "p", "s"])   # searched for a way to choose one random item from a list and found the random method at pynative.com
+    print(f"computer choise is: {computer_choice}")
+
+
 
 starting_game()
