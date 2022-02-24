26573ea9e8d8254b68f01812914a433c3e00a151
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-02-24 14:15:45 +0000

    try to fix bug preventing try and except statment from working correctly

--- run.py
+++ run.py
@@ -10,26 +10,17 @@ def starting_game():
     print("rules: rock beat scissors, paper beats rock and scissors beats paper. \n")
     print("Play by entering r for rock, p for paper and s for scissors.")
 
-    moves = ["r", "p", "s"]
-    player_input = input("Enter your choice: ")
-    computer = random.choice(["r", "p", "s"]) #searched for a way to choose one random item from a list and found the random method at pynative.com
-    print(f"computer choise is: {computer}")
-
-
-def valedating_input():
-    """
-    trying for correct value otherwise return Error
-    """
     try:
-        if player_input == move in moves:
+        player_input = input("Enter your choice: ")
+        if player_input == ("r" or "p" or "s"):
             print(f"You chose {player_input}")
+        # need to call function to continue game later
         else:
             pass
-    except ValueError:
-       print(f"Invalid data input: {player_input}, Please enter r for rock p for paper or s for scissors!")
+    except ValueError as e:
+        print(f"Invalid data input: {e}, Please enter r for rock p for paper or s for scissors!")
 
-
+    computer = random.choice(["r", "p", "s"])   # searched for a way to choose one random item from a list and found the random method at pynative.com
+    print(f"computer choise is: {computer}")
 
 starting_game()
-valedating_input()
-
