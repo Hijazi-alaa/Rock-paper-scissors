2174521ea0ae821fefe409925a6d81919f5a021a
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-02-25 10:57:44 +0000

    add while statment so the game continue after player has submited an input

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 import random
 
+
 def main():
+    """
+    main function to run the game and include other function inside
+    """
 
     print("Welcome to the game of Rock, paper and scissors.\n")
     print("rules: rock beat scissors, paper beats rock and scissors beats paper. \n")
@@ -12,30 +16,30 @@ def main():
         function to calculate winner on each turn
         """
         if (player == "r" and opponent == "s") or (player == "s" and opponent == "p") \
-        or (player == "p" and opponent == "r"):
+            or (player == "p" and opponent == "r"):
             return True
 
     try:
-        available_choices = ["r", "p", "s"]
-        player_input = input("Enter your choice: ")
-        computer_choice = random.choice(["r", "p", "s"])  # searched for a way to choose one random item from a list and found the random method at pynative.com
-        print(f"computer chose: {computer_choice}")
-        if any(player_input in choice for choice in available_choices):
-            print(f"You chose {player_input}")
-            is_winner(player_input, computer_choice)
-            if player_input == computer_choice:
-                print("It's a tie")
-            elif is_winner(player_input, computer_choice):
-                print("You won!")
-            elif is_winner(computer_choice, player_input):
-                print("You lost!")
-        else:
-            raise ValueError()
+        while True:
+            available_choices = ["r", "p", "s"]
+            player_input = input("Enter your choice: ")
+            # searched for a way to choose one random item from a list and found
+            # the random method at pynative.com
+            computer_choice = random.choice(["r", "p", "s"])
+            print(f"computer chose: {computer_choice}")
+            if any(player_input in choice for choice in available_choices):
+                print(f"You chose {player_input}")
+                is_winner(player_input, computer_choice)
+                if player_input == computer_choice:
+                    print("It's a tie")
+                elif is_winner(player_input, computer_choice):
+                    print("You won!")
+                elif is_winner(computer_choice, player_input):
+                    print("You lost!")
+            else:
+                raise ValueError()
     except ValueError:
         print(f"Invalid data input: {player_input}, is not a valid input.\n Please enter r for rock p for paper or s for scissors!")
-
-
-    
 
 
 main()
